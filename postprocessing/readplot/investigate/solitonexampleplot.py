661fb193b949de2f6492d6c3608b06f05d5105b6
jordanpitt3141 <jordanpitt3141@gmail.com>
2015-11-10 13:13:41 +1100

    Update my paper and new notes for how FEM/FVM scheme works

---
+++ postprocessing/readplot/investigate/solitonexampleplot.py
@@ -0,0 +1,40 @@
+import csv
+from numpy.linalg import norm
+from scipy import *
+from pylab import plot, show, legend,xlim,ylim,savefig,title,xlabel,ylabel,clf, loglog
+
+
+#wdir = "../../../../data/raw/Joebigsmooth/o1/13/11/"
+wdir = "../../../../../../PhDold/project/code/data/Joe/alldb/o3/"
+
+
+gap = 1
+
+h0 = 0.1
+g = 9.81
+
+         
+s = wdir + "11.txt"
+with open(s,'r') as file1:
+    readfile = csv.reader(file1, delimiter = ',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        
+    h = []
+    u = []
+    x = []
+    ht = []
+    ut = []
+    j = -1
+    for row in readfile:       
+        if (j >= 0):
+            dx =float(row[0])
+            dt =float(row[1])
+            t =float(row[2])
+            x.append(float(row[3]))
+            h.append(float(row[4]))
+            u.append(float(row[6]))
+                
+        j = j + 1
+    x = array(x)
+    u = array(u)
+    h = array(h)
+   
